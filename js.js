aef63dfb962e81bdc90cd62c7680b6fe3819cd12
rohannikale <rohanpadit745@gmail.com>
2022-10-21 19:15:56 +0530

    added delete button

--- js.js
+++ js.js
@@ -1,13 +1,12 @@
 let update = (data) => {
-    
     let str = ""
     for (i in data) {
-        console.log(data[i])
-        str += `                <div class="note">
+        str += `<div class="note">
         <div class="note-text">
         ${data[i].text}
         </div>
         <div class="btn">
+        <!-- <button class="note-button" onclick="edit_note(${i})"><i class="fa-solid fa-pen-to-square"></i></button> -->
             <button class="note-button"onclick="delete_note(${i})"><i class="fa-solid fa-trash"></i></button>
         </div>
     </div>
@@ -15,42 +14,55 @@ let update = (data) => {
     }
     console.log(str)
     document.getElementsByClassName('content')[0].innerHTML = str
-    
+
 }
 
-let delete_note=(value)=>{
+let delete_note = (value) => {
     let notes = JSON.parse(localStorage.getItem('data'))
-    notes.splice(value,1)    
+    notes.splice(value, 1)
     let data = JSON.stringify(notes)
     localStorage.setItem('data', data)
     update(JSON.parse(localStorage.getItem('data')))
 }
-let edit_note=(value)=>{
-    console.log('hahah')
-}
+
+
+
+
+// let edit_note=(value)=>{
+//     let str=`<div class="input-box">
+//     <textarea name="todo" placeholder="Add Note" id="input" cols="" rows="10"></textarea><br>
+//     <button id="Edit">Add</button>
+// </div>`
+// }
+
+
+
+
 document.getElementById('add').onclick = () => {
-    if (localStorage.getItem('data') == null) {
-        arr = []
-        console.log('adding')
-        let value = document.getElementById('input').value
-        console.log(value)
-        arr.push({ text: value })
-        let data = JSON.stringify(arr)
-        localStorage.setItem('data', data)
-        update(JSON.parse(localStorage.getItem('data')))
-        
-    }
-    else {
-        arr = JSON.parse(localStorage.getItem('data'))
-        let value = document.getElementById('input').value
-        console.log(value)
-        arr.push({ text: value })
-        let data = JSON.stringify(arr)
-        localStorage.setItem('data', data)
-        
-        update(JSON.parse(localStorage.getItem('data')))
+    let value = document.getElementById('input').value
+    if (value.length > 0){
+        if (localStorage.getItem('data') == null) {
+            arr = [].reverse()
+            console.log('adding')
+            console.log(value)
+            arr.push({ text: value })
+            let data = JSON.stringify(arr)
+            localStorage.setItem('data', data)
+            update(JSON.parse(localStorage.getItem('data')))
+
+        }
+        else {
+            arr = JSON.parse(localStorage.getItem('data')).reverse()
+            console.log(value)
+            arr.push({ text: value })
+            let data = JSON.stringify(arr)
+            localStorage.setItem('data', data)
+
+            update(JSON.parse(localStorage.getItem('data')))
+        }
     }
 
-    
+
+
 }
 update(JSON.parse(localStorage.getItem('data')))
